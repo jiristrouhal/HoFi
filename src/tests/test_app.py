39d3961a11b14c00bb10963e13eba17971d2dc6e
M3ezon <MznStrouhal@gmail.com>
2023-08-01 11:54:27 +0200

    Make app handle removing nonexistent branch from the tree

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -27,15 +27,23 @@ class Test_Creating_Tree(unittest.TestCase):
 
     def setUp(self) -> None:
         self.tree = app.Tree()
+        self.tree.add_branch(app.Branch(name="Branch 1", weight=50, length=120))
 
     def test_add_branch_to_a_tree(self):
-        self.tree.add_branch(app.Branch(name="Branch 1", weight=50, length=120))
         self.assertListEqual(self.tree.branches, ["Branch 1"])
 
-    def test_remove_branch_from_a_tree(self):
-        self.tree.add_branch(app.Branch(name="Branch 1", weight=50, length=120))
-        self.tree.remove_branch("Branch 1")
-        self.assertListEqual(self.tree.branches, [])
+    def test_remove_branch_from_the_tree(self):
+        self.tree.add_branch(app.Branch(name="Branch 2", weight=50, length=120))
+        removed_branch = self.tree.remove_branch("Branch 2")
+        self.assertListEqual(self.tree.branches, ["Branch 1"])
+        self.assertTrue(removed_branch is not None)
+        self.assertEqual(removed_branch.name, "Branch 2")
+
+    def test_removing_nonexistent_branch_does_not_affect_the_tree(self):
+        self.tree.add_branch(app.Branch(name="Branch 2", weight=50, length=120))
+        removed_branch = self.tree.remove_branch("Branch 3")
+        self.assertListEqual(self.tree.branches, ["Branch 1", "Branch 2"])
+        self.assertTrue(removed_branch is None)
 
 
 if __name__=="__main__": unittest.main()
