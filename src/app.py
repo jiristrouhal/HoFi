f55ec4a7cf622df05a17acb3fcf90ecb53b3f3a2
M3ezon <MznStrouhal@gmail.com>
2023-08-01 10:46:52 +0200

    Add method for reading the tree from xml

--- src/app.py
+++ src/app.py
@@ -1,3 +1,8 @@
 import tkinter
+import xml.etree.ElementTree as et
+
+
+def read_tree(path:str)->et.ElementTree:
+    return et.parse(path)
 
 
--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -3,6 +3,7 @@ sys.path.insert(1,"src")
 
 import app
 import unittest
+import xml.etree.ElementTree as et
 
 
 class Test_Nothing(unittest.TestCase):
@@ -11,3 +12,17 @@ class Test_Nothing(unittest.TestCase):
         self.assertTrue(True)
 
 
+class Test_Reading_XML_File(unittest.TestCase):
+
+    def setUp(self) -> None:
+        root = et.Element("SomeRootElement")
+        et.SubElement(root,"SomeElement")
+        et.ElementTree(root).write("./somexml.xml")
+
+    def test_opening_the_existing_data(self):
+        tree = app.read_tree("./somexml.xml")
+        self.assertTrue(tree.getroot() is not None)
+        self.assertTrue(tree.getroot().find("SomeElement") is not None)
+
+
+if __name__=="__main__": unittest.main()
