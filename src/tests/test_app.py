5abc9979a1f27105153bcaf92b69e0f26a88b8f2
M3ezon <MznStrouhal@gmail.com>
2023-08-01 07:39:11 +0200

    Add empty test

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -5,7 +5,9 @@ import app
 import unittest
 
 
+class Test_Nothing(unittest.TestCase):
+
+    def test_nothing(self):
+        self.assertTrue(True)
 
 
-if __name__=="__main__": 
-    unittest.main()
