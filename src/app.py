9ba79048c53729ca662877b9f835bcca8260e854
M3ezon <MznStrouhal@gmail.com>
2023-08-02 13:15:47 +0200

    Add method to create new empty tree object

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import tree
+from typing import List, Dict
+
+
+class App:
+
+    def __init__(self)->None:
+        self._trees:Dict[str,tree.Tree] = dict()
+    
+    @property
+    def trees(self)->List[str]: return [t for t in self._trees.keys()]
+
+    def new_tree(self,name:str)->None: self._trees[name] = tree.Tree()
--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -5,9 +5,14 @@ import app
 import unittest
 
 
-class Test_Dummy(unittest.TestCase):
+class Test_Managing_Trees(unittest.TestCase):
 
-    def test_nothing(self): self.assertTrue(True)
+    def setUp(self) -> None:
+        self.app_instance = app.App()
 
+    def test_create_new_tree(self):
+        self.app_instance.new_tree("Tree 1")
+        self.assertListEqual(self.app_instance.trees,["Tree 1"])
+                             
 
 if __name__=="__main__": unittest.main()
