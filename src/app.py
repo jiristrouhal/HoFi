38219440ef45bbd95478eee6ea8904f20fe1728f
M3ezon <MznStrouhal@gmail.com>
2023-08-02 13:27:33 +0200

    Prevent renaming trees to existing names

--- src/app.py
+++ src/app.py
@@ -11,7 +11,15 @@ class App:
     def trees(self)->List[str]: return [t for t in self._trees.keys()]
 
     def new_tree(self,name:str)->None: 
+        if name in self._trees: # do not create new tree, if the tree name is already taken
+            return
         self._trees[name] = tree.Tree()
 
     def remove_tree(self,name:str)->None: 
         self._trees.pop(name)
+
+    def rename_tree(self,old_name:str,new_name:str)->None:
+        if old_name!=new_name and new_name in self._trees: 
+            # tree cannot be renamed, if the new name is taken by other tree
+            return
+        self._trees[new_name] = self._trees.pop(old_name)
--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -23,6 +23,16 @@ class Test_Managing_Trees(unittest.TestCase):
         self.app_instance.remove_tree("Tree 2")
         self.assertListEqual(self.app_instance.trees,[])
 
+    def test_renaming_tree(self):
+        self.app_instance.new_tree("Tree 1")
+        self.app_instance.rename_tree("Tree 1","Tree X")
+        self.assertListEqual(self.app_instance.trees,["Tree X"])
+
+    def test_renaming_tree_to_already_taken_name_will_not_take_effect(self):
+        self.app_instance.new_tree("Tree 1")
+        self.app_instance.new_tree("Tree 2")
+        self.app_instance.rename_tree("Tree 2","Tree 1")
+        self.assertListEqual(self.app_instance.trees,["Tree 1","Tree 2"])
                              
 
 if __name__=="__main__": unittest.main()
