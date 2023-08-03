0fcb34782f575dbdedeb49752bdea6409b25530f
M3ezon <MznStrouhal@gmail.com>
2023-08-03 12:40:13 +0200

    Enable adding tree to the treeview

--- src/tests/test_treeview.py
+++ src/tests/test_treeview.py
@@ -2,14 +2,19 @@ import sys
 sys.path.insert(1,"src")
 
 
-import treeview
+import treeview 
+from tree import Tree
 import unittest
 
 
-class Test_Nothing(unittest.TestCase):
+class Test_Empty_Trees(unittest.TestCase):
 
-    def test_nothing(self): 
-        self.assertTrue(True)
+    def setUp(self) -> None:
+        self.view = treeview.Treeview()
+
+    def test_adding_single_tree(self):
+        self.view.add_tree(Tree("Tree 1"))
+        self.assertEqual(self.view.trees, ("Tree 1",))
 
 
 if __name__=="__main__": unittest.main()
---
+++ src/treeview.py
@@ -0,0 +1,17 @@
+import tkinter.ttk as ttk
+import tkinter as tk
+from typing import Tuple
+import tree as treemod
+
+
+class Treeview:
+
+    def __init__(self, parent:tk.Widget|None = None)->None:
+        self.__widget = ttk.Treeview(parent)
+
+    @property
+    def trees(self)->Tuple[str,...]: 
+        return self.__widget.get_children("")
+    
+    def add_tree(self,tree:treemod.Tree)->None: 
+        self.__widget.insert("","end",iid=tree.name)
