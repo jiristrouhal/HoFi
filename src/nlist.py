c80a65a0446a6c196d98d67f5e61eb8f3e6905a7
M3ezon <MznStrouhal@gmail.com>
2023-08-11 23:02:42 +0200

    Add selection and actions

--- src/nlist.py
+++ src/nlist.py
@@ -1,4 +1,4 @@
-from typing import List, Protocol, Callable, Tuple
+from typing import List, Protocol, Callable
 
 
 class _NItem(Protocol):
@@ -12,12 +12,34 @@ class NamedItemsList:
     def __init__(self)->None:
         self.__items:List[_NItem] = list()
         self.__name_warnings:List[Callable[[str],None]] = list()
+        self.__selection:List[str] = list()
+
+        self.__on_removal:List[Callable[[str],None]] = list()
+        self.__on_adding:List[Callable[[str],None]] = list()
+        self.__on_selecting:List[Callable[[str],None]] = list()
+        self.__on_deselecting:List[Callable[[str],None]] = list()
+
+    @property
+    def selected_names(self)->List[str]: return self.__selection
+    
+    def add_action_on_removal(self,action:Callable[[str],None])->None:
+        self.__on_removal.append(action)
+
+    def add_action_on_adding(self,action:Callable[[str],None])->None:
+        self.__on_adding.append(action)
+
+    def add_action_on_selecting(self,action:Callable[[str],None])->None:
+        self.__on_selecting.append(action)
+
+    def add_action_on_deselecting(self,action:Callable[[str],None])->None:
+        self.__on_deselecting.append(action)
 
     def append(self,*items:_NItem)->None: 
         already_taken_names:List[str] = list()
         for item in items: 
             if self.item(item.name) is None: 
                 self.__items.append(item)
+                for action in self.__on_adding: action(item.name)
             else:
                 already_taken_names.append(item.name)
         if already_taken_names:
@@ -28,11 +50,20 @@ class NamedItemsList:
         for item in self.__items:
             if name==item.name: return item
         return None
+    
+    def select(self,name:str)->None:
+        if name not in self.names: return
+        self.__selection.append(name)
+
+    def deselect(self,name:str)->None:
+        if name not in self.__selection: return
+        self.__selection.remove(name)
 
     def remove(self,name:str)->None:
         item = self.item(name)
         if item is None: return
         self.__items.remove(item)
+        for action in self.__on_removal: action(name)
 
     def add_name_warning(self,warning_action:Callable[[str],None]):
         self.__name_warnings.append(warning_action)
---
+++ src/tests/test_nlist.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.insert(1,"src")
+
+
+import src.nlist as nl
+import unittest
+
+
+class NamedThing: 
+    def __init__(self,name:str)->None:
+        self.name = name
+
+
+class Test_Modifying_List(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.nlist = nl.NamedItemsList()
+        
+    
+    def test_adding_to_list(self)->None:
+        self.nlist.append(NamedThing("Item 1"), NamedThing("Item 2"))
+        self.assertListEqual(self.nlist.names, ["Item 1", "Item 2"])
+
+    def test_removing_from_list(self)->None:
+        self.nlist.append(NamedThing("Item 1"), NamedThing("Item 2"))
+        self.nlist.remove("Item 1")
+        self.assertListEqual(self.nlist.names, ["Item 2"])
+
+    def test_removing_nonexistent_item_has_no_effect(self)->None:
+        self.nlist.append(NamedThing("Item X"))
+        self.nlist.remove("Nonexistent item")
+        self.assertListEqual(self.nlist.names, ["Item X"])
+
+    def test_adding_item_with_already_existing_name_makes_the_list_to_run_warning_action(self):
+
+        self.produced_warnings = list()
+        # create an example of a warning, that the name of the item is already taken
+        def warning_action(*names:str): 
+            for name in names: 
+                self.produced_warnings.append(f"Name '{name}' is already taken.")
+
+        self.nlist.add_name_warning(warning_action)
+
+        self.nlist.append(NamedThing("Item X"))
+        self.nlist.append(NamedThing("Item X"))
+        self.assertListEqual(self.nlist.names, ["Item X"])
+        self.assertListEqual(self.produced_warnings, ["Name 'Item X' is already taken."])
+
+    def test_action_on_adding_tree(self):
+        self.x = ""
+        def action(name): 
+            self.x = "Item " + name + " was added."
+        self.nlist.add_action_on_adding(action)
+        self.nlist.append(NamedThing("XYZ"))
+        self.assertEqual(self.x,  "Item XYZ was added.")
+
+    def test_action_on_removal(self):
+        self.x = ""
+        def action(name): 
+            self.x = "Item " + name + " was removed."
+        
+        self.nlist.add_action_on_adding(action)
+        self.nlist.append(NamedThing("XYZ"))
+        self.nlist.remove("XYZ")
+        self.assertEqual(self.x,  "Item XYZ was removed.")
+
+    def test_action_on_selecting(self):
+        self.selected = ""
+        def action(name): self.selected = name
+        
+        self.nlist.add_action_on_selecting(action)
+        self.nlist.append(NamedThing("Item A"))
+        self.nlist.select("Item A")
+        self.assertListEqual(self.nlist.selected_names,["Item A"])
+
+
+if __name__=="__main__": unittest.main()
