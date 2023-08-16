96b66bada9db1afbc9b8acebacf7d76c1f3c04b2
M3ezon <MznStrouhal@gmail.com>
2023-08-16 09:34:11 +0200

    Merge name and value Labels in Properties window

--- src/properties.py
+++ src/properties.py
@@ -11,6 +11,7 @@ class Properties:
         self.widget = tk.Frame(master)
         self.props:Dict[str,tk.Label] = OrderedDict()
         self.displayed_item:treemod.TreeItem|None = None
+        self.__configure_win()
 
     def display(self,item:treemod.TreeItem)->None:
         self.__draw_properties(item.attributes)
@@ -19,13 +20,9 @@ class Properties:
     def __draw_properties(self, properties:Dict[str,str])->None:
         row = 0
         for name, value in properties.items():
-            label = tk.Label(self.widget,text=name)
-            value_widget = tk.Label(self.widget,text=value)
-
-            self.props[name] = value_widget
-
-            label.grid(row=row,column=0)
-            value_widget.grid(row=row,column=1)
+            label = tk.Label(self.widget,text=name+": "+value)
+            self.props[name] = tk.Label(self.widget,text=name+": "+value)
+            label.grid(row=row)
             row += 1
 
     def clear(self)->None:
@@ -33,5 +30,9 @@ class Properties:
         for child in self.widget.winfo_children(): child.destroy()
         self.props.clear()
 
+    def pack(self,*args,**kwargs)->None:
+        self.widget.pack(*args,**kwargs)
 
+    def __configure_win(self)->None:
+        pass
 
--- src/tests/test_properties.py
+++ src/tests/test_properties.py
@@ -14,14 +14,13 @@ class Test_Displaying_Tree_Properties(unittest.TestCase):
     def setUp(self) -> None:
         tree = TreeItem("Tree 1", {"weight": 4500, "height": 12})
         self.prop_window = pp.Properties()
-        self.prop_window.__draw_properties(tree)
+        self.prop_window.display(tree)
     
-
     def test_displayed_properties_agree_with_the_current_attributes(self)->None:
         self.assertEqual(len(self.prop_window.props),3)
-        self.assertEqual(self.prop_window.props["name"].cget("text"), "Tree 1")
-        self.assertEqual(self.prop_window.props["weight"].cget("text"), "4500")
-        self.assertEqual(self.prop_window.props["height"].cget("text"), "12")
+        self.assertEqual(self.prop_window.props["name"].cget("text"), "name: Tree 1")
+        self.assertEqual(self.prop_window.props["weight"].cget("text"), "weight: 4500")
+        self.assertEqual(self.prop_window.props["height"].cget("text"), "height: 12")
 
     def test_clear_displayed_properties(self)->None:
         self.prop_window.clear()
