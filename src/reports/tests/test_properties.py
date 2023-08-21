8d888ad3c7cf4fe1e9fa0297683bc168c122ac9f
M3ezon <MznStrouhal@gmail.com>
2023-08-21 17:40:38 +0200

    Item properties can be redrawed without changing the displayed_item of the Properties

--- src/reports/tests/test_properties.py
+++ src/reports/tests/test_properties.py
@@ -9,19 +9,29 @@ import src.core.tree as tr
 
 class Test_Displaying_Info_Repeatedly(unittest.TestCase):
 
-    def test_no_change_in_properties_if_already_displayed_item_is_claimed_to_be_identical_to_the_current_item(self)->None:
-        prop_window = pp.Properties()
+    def setUp(self) -> None:
+        self.prop_window = pp.Properties()
         tr.tt.clear()
         tr.tt.add(tr.tt.NewTemplate("Tree", {"name":"NewTree"}, children=()))
-        treeA = tr.Tree("TreeA",tag="Tree")
-        prop_window.displayed_item = treeA
-        self.assertDictEqual(prop_window.props, {})
-        prop_window.display(treeA)
-        self.assertDictEqual(prop_window.props, {})
+        self.treeA = tr.Tree("TreeA",tag="Tree")
+
+    def test_no_change_in_properties_if_already_displayed_item_is_claimed_to_be_identical_to_the_current_item(self)->None:
+        self.prop_window.displayed_item = self.treeA
+        self.assertDictEqual(self.prop_window.props, {})
+        self.prop_window.display(self.treeA)
+        self.assertDictEqual(self.prop_window.props, {})
         # But if the displayed item is set to some other value (e.g., None), change occurs
-        prop_window.displayed_item = None
-        prop_window.display(treeA)
-        self.assertEqual(prop_window.props["name"].cget("text"),"TreeA (tree)")
+        self.prop_window.displayed_item = None
+        self.prop_window.display(self.treeA)
+        self.assertEqual(self.prop_window.props["name"].cget("text"),"TreeA (tree)")
+
+    def test_redrawing_properties(self):
+        self.prop_window.display(self.treeA)
+        self.assertEqual(self.prop_window.props["name"].cget("text"),"TreeA (tree)")
+        self.treeA.rename("TreeB")
+        self.prop_window.redraw()
+        self.assertEqual(self.prop_window.props["name"].cget("text"),"TreeB (tree)")
+
 
 
 if __name__=="__main__": unittest.main()
