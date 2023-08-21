d9804096f4611200926d2c2f73cc07e8dea3727a
M3ezon <MznStrouhal@gmail.com>
2023-08-21 17:52:10 +0200

    Remove unnecessary condition from __draw_properties

--- src/reports/properties.py
+++ src/reports/properties.py
@@ -14,6 +14,7 @@ class Properties:
         self.widget = ttk.Labelframe(master,text=PROPERTIES_TITLE)
         self.props:Dict[str,tk.Label] = OrderedDict()
         self.__configure_win()
+        self.row = 0
 
     def display(self,item:treemod.TreeItem)->None:
         self.clear()
@@ -30,28 +31,22 @@ class Properties:
         tag:str=""
         )->None:
 
-        row = 0
         attr_values = {label:x.value for label,x in attributes.items()}
         dep_attr_values = {label:x.value for label,x in dependent_attributes.items()}
+        attr_values["name"]+= f" ({tag.lower()})"
 
-        if tag.strip()!="": 
-            attr_values["name"]+= f" ({tag.lower()})"
+        self.row = 0
+        self.__draw_attributes(attr_values)
+        self.__draw_attributes(dep_attr_values) 
 
-        for name, value in attr_values.items():
+    def __draw_attributes(self,attributes:Dict[str,treemod._Attribute])->None:
+        for name, value in attributes.items():
             label = tk.Label(self.widget,text="• "+ name+": ")
             value_widget = tk.Label(self.widget,text=str(value))
             self.props[name] = value_widget
-            label.grid(row=row,column=0,sticky=tk.W,padx=(15,0))
-            value_widget.grid(row=row,column=1,sticky=tk.W,padx=(0,15))
-            row += 1
-
-        for name, value in dep_attr_values.items():
-            label = tk.Label(self.widget,text="• "+ name+": ")
-            value_widget = tk.Label(self.widget,text=str(value))
-            self.props[name] = value_widget
-            label.grid(row=row,column=0,sticky=tk.W,padx=(15,0))
-            value_widget.grid(row=row,column=1,sticky=tk.W,padx=(0,15))
-            row += 1
+            label.grid(row=self.row,column=0,sticky=tk.W,padx=(15,0))
+            value_widget.grid(row=self.row,column=1,sticky=tk.W,padx=(0,15))
+            self.row += 1
 
     def clear(self)->None:
         for child in self.widget.winfo_children(): child.destroy()
