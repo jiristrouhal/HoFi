ad2a956983ea7b1f26496406d7a6e665899e1a8b
M3ezon <MznStrouhal@gmail.com>
2023-08-31 10:13:24 +0200

    Test that adding tree item without date attribute does not create any event

--- src/app/tests/test_tree_event_connector.py
+++ src/app/tests/test_tree_event_connector.py
@@ -15,8 +15,9 @@ class Test_Creating_Events_When_Creating_New_Tree_Item(unittest.TestCase):
     def setUp(self) -> None:
         self.app_template = tmpl.AppTemplate(locale_code='en_us',name_attr="name")
         self.app_template.add(
-            tmpl.NewTemplate("Logbook", {"name":"New Logbook"}, children=("Transaction",)),
+            tmpl.NewTemplate("Logbook", {"name":"New Logbook"}, children=("Transaction", "ItemWithoutDateAttribute")),
             tmpl.NewTemplate("Transaction", {"name":"New transaction", "date":"2001-12-25"}, children=()),
+            tmpl.NewTemplate("ItemWithoutDateAttribute", {"name":"New item"}, children=()),
         )
 
         self.editor = tree_editor.TreeEditor(self.app_template)
@@ -37,6 +38,11 @@ class Test_Creating_Events_When_Creating_New_Tree_Item(unittest.TestCase):
     def test_dismissing_event_when_item_is_removed(self)->None:
         self.logbook.new("Transaction 1", tag="Transaction")
         self.logbook.remove_child("Transaction 1")
+        self.assertSetEqual(self.event_manager.planned, set())
+        self.assertSetEqual(self.event_manager.realized, set())
+
+    def test_creating_item_without_date_attribute_does_not_produce_any_event(self)->None:
+        self.logbook.new("ItemWithoutDateAttribute", tag="ItemWithoutDateAttribute")
         self.assertSetEqual(self.event_manager.planned, set())
         self.assertSetEqual(self.event_manager.realized, set())
 
