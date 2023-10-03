e5242cab1eae5e113946472d0694a7e7155684f9
M3ezon <MznStrouhal@gmail.com>
2023-10-03 13:44:25 +0200

    Implement Case and Case_Template classes

--- src/core/editor.py
+++ src/core/editor.py
@@ -1,8 +1,47 @@
+from typing import Optional, List, Tuple
+
+
+class Case_Template:
+
+    def __init__(self) -> None:
+        self.__templates:List[str] = list()
+
+    @property
+    def templates(self)->Tuple[str,...]: return tuple(self.__templates)
+
+    def add_template(self, label:str)->None:
+        if label in self.__templates: raise Case_Template.TemplateAlreadyDefined(label)
+        self.__templates.append(label)
+
+    class TemplateAlreadyDefined(Exception): pass
+
+
+class Case:
+
+    def __init__(self,name:str)->None:
+        self.__name = name
+
+    @property
+    def name(self)->str: return self.__name
+
 
 class Editor:
+
+    def __init__(self)->None:
+        self.__case_template:Optional[Case_Template] = None
+
+    def set_case_template(self, case_template:Case_Template)->None:
+        self.__case_template = case_template
+
+    def new_case(self, name:str)->Case:
+        if self.__case_template is None: raise Editor.CaseTemplateNotSet
+        return Case(name)
     
-    def new(self, name:str)->None:
-        pass
+    class CaseTemplateNotSet(Exception): pass
+
+
+def new_case_template()->Case_Template:
+    return Case_Template()
 
 
 def new_editor()->Editor:
--- src/core/tests/test_editor.py
+++ src/core/tests/test_editor.py
@@ -4,14 +4,37 @@ sys.path.insert(1,"src")
 
 
 import unittest
-from src.core.editor import new_editor
+from src.core.editor import new_case_template, new_editor, Editor, Case_Template
 
 
-class Test_Create_New_Case(unittest.TestCase):
+class Test_Creating_Case_Template(unittest.TestCase):
 
-    def test_create_new_case(self):
+    def setUp(self) -> None:
+        self.ctempl = new_case_template()
+
+    def test_adding_item_template(self):
+        self.ctempl.add_template('Item')
+        self.assertEqual(self.ctempl.templates, ('Item',))
+
+    def test_repeatedly_adding_template_with_the_same_name_raises_exception(self)->None:
+        self.ctempl.add_template('Item')
+        self.assertRaises(Case_Template.TemplateAlreadyDefined, self.ctempl.add_template, 'Item')
+
+    
+
+
+class Test_Using_Case_Template_To_Create_Case(unittest.TestCase):
+
+    def test_creating_new_case(self):
         editor = new_editor()
-        newcase = editor.new('Case')
+        ctempl = new_case_template()
+        editor.set_case_template(ctempl)
+        newcase = editor.new_case('New Case')
+        self.assertEqual(newcase.name, 'New Case')
+
+    def test_creating_case_without_specifying_case_template_raises_exception(self)->None:
+        editor = new_editor()
+        self.assertRaises(Editor.CaseTemplateNotSet, editor.new_case, 'New Case')
 
 
 if __name__=="__main__": unittest.main()
