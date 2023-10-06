ab7889925476d971ddc78c25e8ef0db2f31e0e83
M3ezon <MznStrouhal@gmail.com>
2023-10-06 17:49:18 +0200

    Build rough version of Timeline object

--- src/core/tests/test_time.py
+++ src/core/tests/test_time.py
@@ -4,49 +4,175 @@ sys.path.insert(1,"src")
 
 
 import unittest
-from src.core.time import create_timeline, blank_timeline_template, Timeline, Timeline_Template
+from typing import List
 import datetime
+from src.core.time import Timeline, Timepoint, TimepointInit
+from src.core.item import ItemCreator
 
 
-class Test_Defining_Timeline(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.tt = blank_timeline_template()
-        self.tt.addvar('x',self.tt.attr.integer(5))
-
-    def test_defining_timeline_template(self):
-        self.assertRaises(
-            Timeline_Template.VariableAlreadyDefined, 
-            self.tt.addvar, 'x', self.tt.attr.real(7)
-        )
-
-    def test_applying_timeline_template(self):
-        tline = create_timeline(self.tt)
-        self.assertTrue(tline.has_var('x'))
-        self.assertFalse(tline.has_var('y'))
-        
-    def test_added_variable_retains_its_original_value_if_unaffected_by_any_other_object_or_operation(self):
-        tline = create_timeline(self.tt)
-        tline.set_init('x',3)
-        self.assertEqual(tline('x',datetime.date(2021,11,28)), 3)
-        self.assertEqual(tline('x',datetime.date(2022,11,28)), 3)
-        self.assertEqual(tline('x',datetime.date(1999,11,28)), 3)
-
-    def test_accessing_nonexistent_variable_raises_exception(self)->None:
-        tline = create_timeline(self.tt)
-        self.assertRaises(Timeline.UndefinedVariable, tline, 'y', datetime.date(2021,2,23))
-
-
-class Test_Adding_Item_To_Timeline(unittest.TestCase):
+class Test_Creating_Timeline_And_Timepoints(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.tt = blank_timeline_template()
-        self.tt.addvar('y',self.tt.attr.integer(5))
+        self.cr = ItemCreator()
+        self.root = self.cr.new('Root')
+        self.tline = Timeline(self.root, self.cr._attrfac, timelike_attr_label = 'date', timelike_attr_type='date')
+
+    def test_defining_timeline_without_any_event_yields_empty_dict_of_timepoints(self):
+        self.assertDictEqual(self.tline.timepoints, {})
+
+    def test_passing_item_with_timelike_attribute_does_not_create_any_timepoint_if_timeline_does_not_know_about_the_timelike_attribute(self):
+        item = self.cr.new('Item')
+        self.root.adopt(item)
+        self.assertDictEqual(self.tline.timepoints, {})
+
+    def test_after_specifying_timelike_attribute_label_the_timeline_creates_timepoint_for_every_added_item_with_new_value_of_timelike_attribute(self):
+        item = self.cr.new('Item', {'date':'date'})
+        the_date = datetime.date(2021,12,18)
+        item.set('date', the_date)
+        self.root.adopt(item)
+        self.assertTrue(the_date in self.tline.timepoints)
+        self.cr.undo()
+        self.assertFalse(the_date in self.tline.timepoints)
+        self.cr.redo()
+        self.assertTrue(the_date in self.tline.timepoints)
+
+    def test_removing_timepoint_when_leaving_only_item_with_timelike_attribute_corresponding_to_the_timepoint(self):
+        item = self.cr.new('Item', {'date':'date'})
+        the_date = datetime.date(2021,12,18)
+        item.set('date', the_date)
+        self.root.adopt(item)
+
+        self.root.leave(item)
+        self.assertFalse(the_date in self.tline.timepoints)
+        self.cr.undo()
+        self.assertTrue(the_date in self.tline.timepoints)
+        self.cr.redo()
+        self.assertFalse(the_date in self.tline.timepoints)
+
+    def test_timepoint_is_not_removed_until_all_items_with_timelike_attribute_corresponding_to_it_are_gone(self):
+        item_A = self.cr.new('Item', {'date':'date'})
+        item_B = self.cr.new('Item', {'date':'date'})
+        the_date = datetime.date(2021,12,18)
+        item_A.set('date', the_date)
+        item_B.set('date', the_date)
+        self.root.adopt(item_A)
+        self.root.adopt(item_B)
+
+        self.root.leave(item_A)
+        self.assertTrue(the_date in self.tline.timepoints)
+        self.root.leave(item_B)
+        self.assertFalse(the_date in self.tline.timepoints)
+
+
+class Test_Init_Timepoint(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.cr = ItemCreator()
+        self.root = self.cr.new('Root')
+        self.tline = Timeline(self.root, self.cr._attrfac, 'date', 'date')
+
+    def test_adding_items_to_init_point_raises_exception(self)->None:
+        init_point = self.tline.pick_point(datetime.date(2023,12,26))
+        item = self.cr.new('Item')
+        self.assertRaises(TimepointInit.CannotAddItem, init_point._add_item, item)
     
-    def test_adding_day_to_timeline(self):
-        tline = create_timeline(self.tt)
-        tline.add_day(datetime.date(2021,9,15))
-        
+    def test_removing_items_from_init_point_raises_exception(self)->None:
+        init_point = self.tline.pick_point(datetime.date(2023,12,26))
+        item = self.cr.new('Item')
+        self.assertRaises(TimepointInit.No_Items_At_Init_Timepoint, init_point._remove_item, item)
+
+
+class Test_Insert_Into_Ordered_List(unittest.TestCase):
+
+    def test_insert_into_empty_list(self)->None:
+        thelist:List[float] = []
+        Timeline.insert(5,thelist)
+        self.assertListEqual(thelist, [5])
+
+    def test_insert_value_larger_than_list_max(self)->None:
+        thelist:List[float] = [7,8]
+        Timeline.insert(10,thelist)
+        self.assertListEqual(thelist, [7,8,10])
+
+    def test_insert_value_smaller_than_list_min(self)->None:
+        thelist:List[float] = [7,8]
+        Timeline.insert(5,thelist)
+        self.assertListEqual(thelist, [5,7,8])
+
+    def test_inserting_already_present_value_has_no_effect(self)->None:
+        thelist:List[float] = [7,8]
+        Timeline.insert(7,thelist)
+        self.assertListEqual(thelist, [7,8])
+
+    def test_inserting_value_between_two_list_values(self)->None:
+        thelist:List[float] = [7,8]
+        Timeline.insert(7.9, thelist)
+        self.assertListEqual(thelist, [7, 7.9, 8])
+
+
+class Test_Picking_Timepoints(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.cr = ItemCreator()
+        self.root = self.cr.new('Root')
+        self.tline = Timeline(self.root, self.cr._attrfac, 'date', 'date')
+
+    def test_init_timepoint_is_always_picked_if_no_items_were_added_to_root(self)->None:
+        init_point = self.tline.pick_point(datetime.date(2023,12,26))
+        self.assertTrue(init_point.is_init())
+        self.assertEqual(self.tline.pick_point(datetime.date(2123,12,26)), init_point)
+        self.assertEqual(self.tline.pick_point(datetime.date(1823,12,26)), init_point)
+
+    def test_latest_point_before_or_at_given_time_is_picked_if_specified_time_is_at_or_after_first_timepoint(self):
+        self.tline._add_timepoint(datetime.date(1900,3,20), self.tline.create_timepoint(self.tline))
+        self.tline._add_timepoint(datetime.date(2000,3,20), self.tline.create_timepoint(self.tline))
+        point1 = self.tline.timepoints[datetime.date(1900,3,20)]
+        point2 = self.tline.timepoints[datetime.date(2000,3,20)]
+        self.assertEqual(self.tline.pick_point(datetime.date(2100,3,20)), point2)
+        self.assertEqual(self.tline.pick_point(datetime.date(2000,3,20)), point2)
+        self.assertEqual(self.tline.pick_point(datetime.date(1950,3,20)), point1)
+        self.assertEqual(self.tline.pick_point(datetime.date(1900,3,20)), point1)
+        self.assertTrue(self.tline.pick_point(datetime.date(1800,3,20)).is_init())
+
+
+class Test_Timeline_Variable(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.cr = ItemCreator()
+        self.root = self.cr.new('Root')
+        self.tline = Timeline(self.root, self.cr._attrfac, 'date', 'date', tvars={'y':self.cr.attr.integer(3)})
+
+    def test_adding_timeline_variable_without_any_timepoints_always_returns_the_initial_value(self):
+        self.assertEqual(self.tline('y',datetime.date(1491,4,20)), 3)
+        self.assertEqual(self.tline('y',datetime.date(2021,10,18)), 3)
+        self.assertEqual(self.tline('y',datetime.date(2081,6,21)), 3)
+    
+    def test_unbound_timeline_variable_is_unaffected_by_adding_any_timepoints(self):
+        item = self.cr.new('Item', {'date':'date', 'x':'integer'})
+        item.set('x',5)
+        item.set('date',datetime.date(2023,10,15))
+        self.root.adopt(item)
+        self.assertTrue(len(self.tline.timepoints)==1)
+        self.assertEqual(self.tline('y',datetime.date(2023,10,15)), 3)
+
+    def test_binding_timeline_variable(self):
+        def add_sum_of_x(y:int, x:List[int])->int:
+            return y+sum(x)
+        self.tline.bind('y', add_sum_of_x, '[x:integer]')
+
+        itemA = self.cr.new('Item', {'date':'date', 'x':'integer'})
+        itemB = self.cr.new('Item', {'date':'date', 'x':'integer'})
+        itemA.set('x',5)
+        itemB.set('x',2)
+        itemA.set('date',datetime.date(2023,10,15))
+        itemB.set('date',datetime.date(2023,10,17))
+        self.root.adopt(itemA)
+        self.root.adopt(itemB)
+
+        self.assertEqual(self.tline('y',datetime.date(2023,10,14)), 3)
+        self.assertEqual(self.tline('y',datetime.date(2023,10,15)), 8)
+        self.assertEqual(self.tline('y',datetime.date(2023,10,16)), 8)
+        self.assertEqual(self.tline('y',datetime.date(2023,10,17)), 10)
 
 
 if __name__=="__main__": unittest.main()
--- src/core/time.py
+++ src/core/time.py
@@ -1,49 +1,275 @@
-import datetime
-from typing import Any, Dict
-from src.core.attributes import Attribute_Data_Constructor
+from __future__ import annotations
+
+import dataclasses
+from typing import Dict, Any, Callable
+
+from src.core.item import Item, Parentage_Data
+from src.cmd.commands import Command, Empty_Command
+from src.core.attributes import Attribute, Attribute_Factory, Attribute_List
 
 
-class Timeline_Template:
-    def __init__(self)->None:
-        self.__attrc = Attribute_Data_Constructor()
-        self.__vars:Dict[str,Dict[str,Any]] = dict()
+@dataclasses.dataclass
+class Timepoint_Data:
+    tline:Timeline
+    item:Item
+    time:Any
 
-    @property
-    def attr(self)->Attribute_Data_Constructor: return self.__attrc
-    @property
-    def vars(self)->Dict[str,Dict[str,Any]]: return self.__vars.copy()
 
-    def addvar(self,label:str,info:Dict[str,Any])->None:
-        if label in self.__vars: raise Timeline_Template.VariableAlreadyDefined(label)
-        else: self.__vars[label] = info
+@dataclasses.dataclass
+class Add_Timepoint(Command):
+    data:Timepoint_Data
+    timepoint:Timepoint = dataclasses.field(init=False)
+    def run(self)->None:
+        if self.data.time not in self.data.tline.timepoints:
+            self.timepoint = self.data.tline.create_timepoint(self.data.time)
+            self.data.tline._add_timepoint(self.data.time, self.timepoint)
+        else:
+            self.timepoint = self.data.tline.timepoints[self.data.time]
+        self.timepoint._add_item(self.data.item)
 
-    class VariableAlreadyDefined(Exception): pass
+    def undo(self)->None:
+        self.timepoint._remove_item(self.data.item)
+        if not self.timepoint.has_items():
+            self.data.tline._remove_timepoint(self.data.time)
+
+    def redo(self)->None:
+        if not self.timepoint.has_items():
+            self.data.tline._add_timepoint(self.data.time, self.timepoint)
+        self.timepoint._add_item(self.data.item)
+
+
+@dataclasses.dataclass
+class Remove_Timepoint(Command):
+    data:Timepoint_Data
+    timepoint:Timepoint = dataclasses.field(init=False)
+    def run(self)->None:
+        self.timepoint = self.data.tline.timepoints[self.data.time]
+        self.timepoint._remove_item(self.data.item)
+        if not self.timepoint.has_items():
+            self.data.tline._remove_timepoint(self.data.time)
+
+    def undo(self)->None:
+        if not self.timepoint.has_items():
+            self.data.tline._add_timepoint(self.data.time, self.timepoint)
+            self.timepoint._add_item(self.data.item)
+
+    def redo(self)->None:
+        self.timepoint._remove_item(self.data.item)
+        if not self.timepoint.has_items():
+            self.data.tline._remove_timepoint(self.data.time)
+
+
+from typing import Tuple, List
+@dataclasses.dataclass(frozen=True)
+class Binding:
+    dependent:str
+    func:Callable[[Any],Any]
+    free:Tuple[str,...]
 
 
 class Timeline:
-
-    def __init__(self, template:Timeline_Template)->None:
-        self.__vars_info = template.vars
     
-    def __call__(self, var_name:str, date:datetime.date)->Any:
-        if var_name not in self.__vars_info: raise Timeline.UndefinedVariable(var_name)
-        return self.__vars_info[var_name]['init_value']
+    def __init__(
+        self, 
+        root:Item, 
+        attribute_factory:Attribute_Factory,
+        timelike_attr_label:str,
+        timelike_attr_type:str,
+        tvars:Dict[str,Dict[str,Any]]={}
+        )->None:
+
+        self.__tlike_label = timelike_attr_label
+        self.__id = str(id(self))
+
+        self.__root = root
+        self.__root.command['adopt'].add(self.__id, self.__new_descendant_of_root, 'post')
+        self.__root.command['leave'].add(self.__id, self.__leaving_of_descendant_of_root, 'pre')
+
+        self.__timepoints:Dict[Any,Timepoint] = {}
+        self.__times:List[Any] = list()
+        self.__vars:Dict[str,Dict[str, Any]] = tvars
+
+        self.__bindings:Dict[str, Binding] = dict()
+        self.__attribute_factory = attribute_factory
+
+        self.__init_point = self.create_timepoint(time=None, init=True)
+
+    @property
+    def timepoints(self)->Dict[str,Timepoint]: return self.__timepoints.copy()
+    @property
+    def var_info(self)->Dict[str,Dict[str,Any]]: return self.__vars.copy()
+    @property
+    def attrfac(self)->Attribute_Factory: return self.__attribute_factory
+    @property
+    def bindings(self)->Dict[str,Binding]: return self.__bindings.copy()
+
+    def bind(self, dependent:str, func:Callable[[Any],Any], *free:str)->None:
+        self.__bindings[dependent] = Binding(dependent, func, free)
+
+    def create_timepoint(self, time:Any, init:bool=False)->Timepoint:
+        vars = self.__create_vars()
+        vars[''] = time
+        if init: return self.__initital_timepoint(vars)
+        else: return self.__regular_timepoint(vars)
+
+    def __create_vars(self)->Dict[str,Attribute]:
+        vars:Dict[str,Attribute] = {}
+        var_info = self.var_info
+        for label, info in var_info.items():
+            vars[label] = self.attrfac.new_from_dict(**info)
+        return vars
+
+    def __initital_timepoint(self,vars:Dict[str,Attribute])->TimepointInit:
+        return TimepointInit(vars)
     
-    def add_day(self, date:datetime.date)->None:
-        pass
-
-    def has_var(self,var_name:str)->bool:
-        return var_name in self.__vars_info
+    def __regular_timepoint(self,vars:Dict[str,Attribute])->TimepointRegular:
+        tpoint = TimepointRegular(vars,self)
+        self.__set_up_timepoint_bindings(tpoint)
+        return tpoint
     
-    def set_init(self,var_name:str, value:Any)->None:
-        self.__vars_info[var_name]['init_value'] = value
+    def __set_up_timepoint_bindings(self, tpoint:TimepointRegular)->None:
+        for b in self.__bindings.values():
+            item_vars:List[Attribute_List] = list()
+            for f in b.free: 
+                if f[0]=='[' and f[-1]==']': 
+                    f_label, f_type = f[1:-1].split(":")
+                    tpoint._add_var_list(f_label, self.attrfac.newlist(f_type))
+                item_vars.append(tpoint.item_var(f_label))
+            
+            time_index = self.__index_of_nearest_smaller(tpoint.var(''),self.__times)
+            if time_index<0:
+                prev_tpoint_var = self.__init_point.var(b.dependent)
+            else:
+                prev_tpoint_var = self.__timepoints[self.__times[time_index-1]].var(b.dependent)
+            tpoint.var(b.dependent).add_dependency(b.func, prev_tpoint_var, *item_vars)
+
+    def pick_point(self, time:Any)->Timepoint:
+        previous_time_of_timepoint = self.__pick_timepoint_time(time)
+        if previous_time_of_timepoint is None: return self.__init_point
+        else: return self.__timepoints[previous_time_of_timepoint]
+
+    def _add_timepoint(self,time:Any, timepoint:Timepoint)->None:
+        if not self.__times: 
+            self.__times.append(time)
+        if time<min(self.__times): 
+            self.__times.insert(0,time)
+        elif time>max(self.__times): 
+            self.__times.append(time)
+        else: 
+            Timeline.insert(time, self.__times)
+        self.__timepoints[time] = timepoint
+            
+    def _remove_timepoint(self,time:Any)->None:
+        self.__timepoints.pop(time)
+        self.__times.remove(time)
+
+    def __new_descendant_of_root(self, data:Parentage_Data)->Command:
+        if not data.child.has_attribute(self.__tlike_label): 
+            return Empty_Command()
+        else: 
+            return Add_Timepoint(
+                Timepoint_Data(self,data.child,data.child(self.__tlike_label))
+            )
+
+    def __leaving_of_descendant_of_root(self, data:Parentage_Data)->Command:
+        time = data.child(self.__tlike_label)
+        return Remove_Timepoint(Timepoint_Data(self,data.child,time))
     
-    class UndefinedVariable(Exception): pass
+    def __pick_timepoint_time(self, time:Any)->Any:
+        if not self.__times or time<self.__times[0]: 
+            return None
+        elif time==self.__times[0]:
+            return self.__times[0]
+        else:
+            return self.__times[self.__index_of_nearest_smaller(time, self.__times)]
+        
+    def __call__(self,variable_label:str, time:Any)->Any:
+        timepoint = self.pick_point(time)
+        return timepoint(variable_label)
+    
+    @staticmethod
+    def insert(x:Any, thelist:List[Any]):
+        insertion_index = Timeline.__index_of_nearest_smaller(x,thelist)+1
+        if insertion_index>=len(thelist):
+            thelist.append(x)
+        elif not x==thelist[insertion_index]: 
+            thelist.insert(insertion_index, x)
+
+    @staticmethod
+    def __index_of_nearest_smaller(x:Any, thelist:List[Any])->int:
+        if not thelist or x<=thelist[0]: 
+            return -1
+        elif thelist[-1]<=x: 
+            return len(thelist)-1
+        else:
+            mid_index = int(len(thelist)/2)
+            if x>thelist[mid_index]: 
+                return mid_index
+            else: 
+                return 0
 
 
-def create_timeline(template:Timeline_Template)->Timeline:
-    return Timeline(template)
+from typing import Set
+import abc
+class Timepoint(abc.ABC):
+    def __init__(self, vars:Dict[str,Attribute])->None:
+        self._items:Set[Item] = set()
+        self.__vars = vars
+
+    def __call__(self,var_label:str)->Any:
+        return self.__vars[var_label].value
+
+    def has_items(self)->bool: return bool(self._items)
+
+    def var(self,label:str)->Attribute: return self.__vars[label]
+
+    @abc.abstractmethod
+    def _add_item(self,item:Item)->None: pass
+
+    @abc.abstractmethod
+    def _remove_item(self,item:Item)->None: pass
+
+    @abc.abstractmethod
+    def is_init(self)->bool: pass
 
 
-def blank_timeline_template()->Timeline_Template:
-    return Timeline_Template()
+class TimepointRegular(Timepoint):
+
+    def __init__(self, vars:Dict[str,Attribute], timeline:Timeline)->None:
+        super().__init__(vars)
+        self.__item_var_lists:Dict[str, Attribute_List] = dict()
+
+  
+    def _add_var_list(self,label:str, varlist:Attribute_List)->None:
+        self.__item_var_lists[label] = varlist
+
+    def _add_item(self,item:Item)->None:
+        self._items.add(item)
+        for attr_label, attr in item.attributes.items():
+            if attr_label in self.__item_var_lists: 
+                self.__item_var_lists[attr_label].append(attr)
+        
+    def _remove_item(self,item:Item)->None:
+        self._items.remove(item)
+
+    def __append_to_var_list(self, item_var_label:str, item_var:Attribute)->None:
+        self.__item_var_lists[item_var_label].append(item_var)
+
+    def __remove_from_var_list(self, item_var_label:str, item_var:Attribute)->None:
+        self.__item_var_lists[item_var_label].remove(item_var)
+
+    def item_var(self,label:str)->Attribute_List:
+        return self.__item_var_lists[label]
+
+    def is_init(self)->bool: return False
+
+
+
+class TimepointInit(Timepoint):
+
+    def _add_item(self,item:Item)->None: raise TimepointInit.CannotAddItem
+    def _remove_item(self,item:Item)->None: raise TimepointInit.No_Items_At_Init_Timepoint
+    def is_init(self)->bool: return True
+
+    class CannotAddItem(Exception): pass
+    class No_Items_At_Init_Timepoint(Exception): pass
