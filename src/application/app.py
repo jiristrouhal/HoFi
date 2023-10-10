3b872e0151b9345a9d5eadbfee41b83e8dea6507
M3ezon <MznStrouhal@gmail.com>
2023-10-10 11:31:47 +0200

    Add application settings

--- src/application/app.py
+++ src/application/app.py
@@ -5,7 +5,13 @@ from src.core.editor import blank_case_template, new_editor, Case_Template, Edit
 from src.core.attributes import Locale_Code
 
 
+from typing import Any, Dict, Literal
+
+
 class App:
+
+    __SettingLabel = Literal['currency']
+
     def __init__(
         self, 
         case_template:Case_Template=blank_case_template(), 
@@ -14,9 +20,17 @@ class App:
 
         self.__locale_code = locale_code
         self.__editor = new_editor(case_template, locale_code)
+        self.__settings:Dict[App.__SettingLabel,Any] = {
+            'currency':"USD"
+        }
 
     @property
     def locale_code(self)->str: return self.__locale_code
     @property
     def editor(self)->Editor: return self.__editor
+
+    def settings(self, label:App.__SettingLabel)->Any: return self.__settings[label]
+
+    def set(self, label:App.__SettingLabel, value:Any)->None:
+        self.__settings[label] = value
     
--- src/application/tests/test_app.py
+++ src/application/tests/test_app.py
@@ -8,10 +8,17 @@ from src.application.app import App
 
 class Test_Setting_App(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.app = App(locale_code = "cs_cz")
+
     def test_setting_localization(self):
-        app = App(locale_code = "cs_cz")
-        self.assertEqual(app.locale_code, "cs_cz")
-        self.assertEqual(app.editor.locale_code, "cs_cz")
+        self.assertEqual(self.app.locale_code, "cs_cz")
+        self.assertEqual(self.app.editor.locale_code, "cs_cz")
+
+    def test_application_settings(self):
+        self.app.set('currency','CZK')
+        self.assertEqual(self.app.settings('currency'),'CZK')
+
 
     
 if __name__=="__main__":  unittest.main()
